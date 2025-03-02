1269b89b66751732dd522811fe083b49340dbb97
xdobx <f77cb@posteo.de>
2025-03-02 14:09:57 +0100

    refactor cli (WIP)

--- paredros-debugger/ParseInformations.py
+++ paredros-debugger/ParseInformations.py
@@ -9,7 +9,14 @@ import sys
 import subprocess
 from antlr4 import InputStream, CommonTokenStream
 from antlr4.tree.Trees import Trees
-from utils import find_grammar_file, rename_grammar_file, generate_parser, modify_generated_parser, load_parser_and_lexer
+from antlr4.tree.Tree import ParseTreeWalker
+from antlr4.atn.PredictionMode import PredictionMode
+
+from LookaheadVisualizer import LookaheadVisualizer
+from DetailedParseListener import DetailedParseListener, resolveLiteralOrSymbolicName
+from UserGrammar import UserGrammar
+
+from utils import find_grammar_file, rename_grammar_file, generate_parser, modify_generated_parser, load_parser_and_lexer, get_start_rule
 
 class NodeWrapper:
     """Wrapper for ANTLR parse tree nodes to enable easy traversal."""
@@ -60,20 +67,40 @@ class ParseInformations:
              input_text = f.read()
         except FileNotFoundError as e:
             raise FileNotFoundError(f"The file {self.input_file} does not exist.") from e
+        print("======= Reading input file =======")
         self.input_text = input_text
-        
+        print("======= Load parser and lexer =======")
         self.lexer_class, self.parser_class = load_parser_and_lexer(self.grammar_folder)
+        print("======= Parsing input text =======")
         self.root = self._parse()
     
     def _parse(self):
         """Runs the parser on the given input text and returns the root parse tree node."""
+        print("input stream")
         input_stream = InputStream(self.input_text)
+        print("lexer")
         lexer = self.lexer_class(input_stream)
+        print("tokens")
         tokens = CommonTokenStream(lexer)
+        print("parser")
         parser = self.parser_class(tokens)
-        start_rule = parser.ruleNames[0]  # Assumes the first rule is the entry point
+
+        parser._interp = LookaheadVisualizer(parser)
+        parser._interp.predictionMode = PredictionMode.LL_EXACT_AMBIG_DETECTION
+        parser.removeErrorListeners()
+        walker = ParseTreeWalker()
+        listener = DetailedParseListener(parser)
+        self.grammar_file = os.path.join(self.grammar_folder, "MyGrammar.g4")
+        grammar = UserGrammar()
+        grammar.add_grammar_file(self.grammar_file)
+        self.rules_dict = grammar.get_rules()
+        start_rule = get_start_rule(self.grammar_file)
+        print("start rule", start_rule)
         parse_method = getattr(parser, start_rule)
         tree = parse_method()
+        self.parse_tree = Trees.toStringTree(tree, None, parser)
+        print("Final Parse Tree")   
+        print(self.parse_tree)
         return NodeWrapper(tree, parser)
     
     def get_root_node(self):
