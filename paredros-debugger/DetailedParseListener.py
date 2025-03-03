e8020dcec6248f8353a1647514dd5190d545e3f0
xdobx <f77cb@posteo.de>
2025-03-03 14:35:36 +0100

    add Detailed Parse Listener

---
+++ paredros-debugger/DetailedParseListener.py
@@ -0,0 +1,28 @@
+from antlr4 import ParseTreeListener
+def resolveLiteralOrSymbolicName(parser, token):
+    name = None
+    if parser.symbolicNames:
+        name = parser.symbolicNames[token.type]
+    if name == "<INVALID>":
+        return f"Literal: {token.text}"
+    else:
+        return f"{parser.symbolicNames[token.type]}: {token.text}"
+
+class DetailedParseListener(ParseTreeListener):
+    def __init__(self, parser):
+        self.parser = parser
+        self.indent = 0
+
+    def enterEveryRule(self, ctx):
+        rule = self.parser.ruleNames[ctx.getRuleIndex()]
+        print(f"{'│ ' * self.indent}┌─ {rule}")
+        self.indent += 1
+
+    def exitEveryRule(self, ctx):
+        self.indent -= 1
+        rule = self.parser.ruleNames[ctx.getRuleIndex()]
+        print(f"{'│ ' * self.indent}└─ {rule}")
+
+    def visitTerminal(self, node):
+        token = resolveLiteralOrSymbolicName(self.parser, node.getSymbol())
+        print(f"{'│ ' * self.indent}├─ {token}")
