a77bc8726611d800f7560e32c5c7b6a7594cc28e
Daniel Motz <hello@danielmotz.de>
2025-05-18 21:51:39 +0200

    #13 Swap expected and actual position in assert statement

--- tests/parse_information_functions.py
+++ tests/parse_information_functions.py
@@ -1,6 +1,5 @@
 import unittest
 
-
 """
 This module tests the interface to paredros. It especially tests, whether the
 ParseInformation class is adequate and exposed.
@@ -18,7 +17,7 @@ class InterfaceTest(unittest.TestCase):
         root = p.get_current_tree_dict()
 
         # the parse tree should be a dict
-        self.assertEqual(type(root), dict)
+        self.assertEqual(dict, type(root))
 
         # pass through the tree and check that all ids are unique
         ids = {}
@@ -32,21 +31,21 @@ class InterfaceTest(unittest.TestCase):
 
         # ---------- ROOT ----------
         # the root node should be startRule and be of type rule
-        self.assertEqual(root["node_type"], "rule", "[SIMPLETON] Root node should be of type rule")
-        self.assertEqual(root["rule_name"], "startRule", "[SIMPLETON] Root node should be startRule")
-        self.assertEqual(root["token"], None, "[SIMPLETON] 'startRule' should not have a token")
+        self.assertEqual("rule", root["node_type"], "[SIMPLETON] Root node should be of type rule")
+        self.assertEqual("startRule", root["rule_name"], "[SIMPLETON] Root node should be startRule")
+        self.assertEqual(None, root["token"], "[SIMPLETON] 'startRule' should not have a token")
         # only one child
-        self.assertEqual(len(root["children"]), 1)
+        self.assertEqual(1, len(root["children"]))
 
         # ---------- FIRST CHILD ----------
         rule_zwoelf = root["children"][0]
         # all nodes have to have different ids
-        self.assertNotEquals(root["id"], rule_zwoelf["id"])
+        self.assertNotEqual(rule_zwoelf["id"], root["id"])
 
         # misleadingly, the rule "zwoelf" matches the string "123"
-        self.assertEqual(rule_zwoelf["rule_name"], "zwoelf", "[SIMPLETON] First child should be rule 'zwoelf'")
-        self.assertEqual(rule_zwoelf["node_type"], "rule", "[SIMPLETON] First child should be of type rule")
-        self.assertEqual(rule_zwoelf["token"], None,  "[SIMPLETON] Rule 'zwoelf' should not have a token")
+        self.assertEqual("zwoelf", rule_zwoelf["rule_name"], "[SIMPLETON] First child should be rule 'zwoelf'")
+        self.assertEqual("rule", rule_zwoelf["node_type"], "[SIMPLETON] First child should be of type rule")
+        self.assertEqual(None, rule_zwoelf["token"],  "[SIMPLETON] Rule 'zwoelf' should not have a token")
 
         # 'zwoelf' can be superseded by:
         # zwoelf -> EINS X,
@@ -57,17 +56,17 @@ class InterfaceTest(unittest.TestCase):
         # the first lexeme should be "EINS"
         eins = rule_zwoelf["children"][0]
         # all nodes have to have different ids
-        self.assertNotEquals(rule_zwoelf["id"], eins["id"])
-        self.assertEqual(eins["node_type"], "lexeme", "[SIMPLETON] First child should be of type lexeme")
-        self.assertEqual(eins["rule_name"], "EINS", "[SIMPLETON] First child should be lexeme 'EINS'")
-        self.assertEqual(eins["token"], "1", "[SIMPLETON] Lexeme 'EINS' should have a token")
+        self.assertNotEqual(eins["id"], rule_zwoelf["id"])
+        self.assertEqual("token", eins["node_type"], "[SIMPLETON] First child should be of type lexeme")
+        self.assertEqual("EINS", eins["rule_name"], "[SIMPLETON] First child should be lexeme 'EINS'")
+        self.assertEqual("1", eins["token"], "[SIMPLETON] Lexeme 'EINS' should have a token")
 
         # ---------- SECOND LEXEME ----------
         # the second lexeme should be "ZWEI"
         zwei = rule_zwoelf["children"][1]
         # all nodes have to have different ids
-        self.assertNotEquals(rule_zwoelf["id"], zwei["id"])
-        self.assertNotEquals(eins["id"], zwei["id"])
+        self.assertNotEqual(zwei["id"], rule_zwoelf["id"])
+        self.assertNotEqual(zwei["id"], eins["id"])
 
 
 if __name__ == '__main__':
